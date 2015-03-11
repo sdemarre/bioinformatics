5182ac6f8f341d0b7e70eef5012952bfd71e7ccb
serge de marre <sdemarre@gmail.com>
2015-03-11 17:35:17 +0100

    add set class for :seto problem

---
+++ rosalind/set.lisp
@@ -0,0 +1,60 @@
+(in-package :rosalind)
+
+(defclass rosalind-set ()
+  ((max-element :accessor max-element :initarg :max-element)
+   (elements :reader elements :initarg :elements :initform (make-hash-table))))
+
+(defmethod add-element ((this rosalind-set) el)
+  (setf (gethash el (elements this)) 1))
+
+(defmethod remove-element ((this rosalind-set) el)
+  (remhash el this))
+
+(defun do-for-set-elements (set fun)
+  (iter (for (key value) in-hashtable (elements set))
+	(funcall fun key)))
+
+(defun has-element-p (set el)
+  (gethash el (elements set)))
+
+(defmacro with-new-set (set-name &body body)
+  `(let ((,set-name (make-instance 'rosalind-set)))
+     ,@body
+     ,set-name))
+
+(defmethod rset-union ((this rosalind-set) other-set)
+  "rosalind-set union"
+  (with-new-set union
+    (do-for-set-elements this #'(lambda (el) (add-element union el)))
+    (do-for-set-elements other-set #'(lambda (el) (add-element union el)))))
+
+(defmethod rset-intersection ((this rosalind-set) other-set)
+  "rosalind-set intersection"
+  (with-new-set intersection
+    (do-for-set-elements this #'(lambda (el) (when (has-element-p other-set el)
+					       (add-element intersection el))))))
+
+(defmethod rset-difference ((this rosalind-set) other-set)
+  "rosalind-set difference"
+  (with-new-set difference
+      (do-for-set-elements this #'(lambda (el) (unless (has-element-p other-set el)
+						 (add-element difference el))))))
+
+(defmethod rset-complement ((this rosalind-set))
+  "rosalind-set complement"
+  (with-new-set complement
+    (iter (for el from 1 to (max-element this))
+	  (unless (has-element-p this el)
+	    (add-element complement el)))))
+
+(defun parse-rosalind-set (max-element line)
+  "{1, 2, 3} -> rosalind-set with elements 1 2 and 3"
+  (with-new-set result
+    (cl-ppcre:do-matches (start end "[0-9]+" line)
+      (add-element result (parse-integer (subseq line start end))))
+    (setf (max-element result) max-element)))
+
+(defun print-set (rosalind-set &optional (stream t))
+  (let (elements)
+    (do-for-set-elements rosalind-set #'(lambda (el) (push el elements)))
+    (format stream "{~{~a~^, ~}}" (nreverse elements))))
